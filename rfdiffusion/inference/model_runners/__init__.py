dd1b2b8f5567c46100a624c304e6c5170139dc71
jos <jvdoorss@gmail.com>
2026-06-08 08:19:06 +0000

    refactor model_runners
    split in
    * sampler
    * scaffolded_sampler

---
+++ rfdiffusion/inference/model_runners/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+from omegaconf import DictConfig
+
+from .sampler import Sampler
+from .scaffolded_sampler import ScaffoldedSampler, SelfConditioning
+
+def sampler_selector(conf: DictConfig) -> Sampler:
+    if conf.scaffoldguided.scaffoldguided:
+        return ScaffoldedSampler(conf)
+    match conf.inference.model_runner:
+        case "default":
+            return Sampler(conf)
+        case "SelfConditioning":
+            return SelfConditioning(conf)
+        case "ScaffoldedSampler":
+            return ScaffoldedSampler(conf)
+        case _ as sampler:
+            raise ValueError(f"Unrecognized sampler {sampler}")
